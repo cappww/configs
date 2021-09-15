010758de9cf79f1588c77fa6dbfabc81b4358060
Capp Wiedenhoefer <33261505+cappww@users.noreply.github.com>
2021-09-15 11:42:30 -0700

    Create install-docker-compose.sh

---
+++ install-docker-compose.sh
@@ -0,0 +1,16 @@
+set -e
+
+echo "Installing docker"
+sudo yum install docker
+
+echo "Adding permissions"
+sudo usermod -a -G docker ec2-user
+
+echo "Installing docker-compose"
+sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) $
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+docker-compose version
+
+echo "done"
