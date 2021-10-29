6bf7cc7d14e9bf671d5307293e195b00fecd22cb
Capp Wiedenhoefer <33261505+cappww@users.noreply.github.com>
2021-10-29 14:13:26 -0700

    Update install-docker-compose.sh

--- install-docker-compose.sh
+++ install-docker-compose.sh
@@ -9,7 +9,7 @@ echo "Installing docker"
 sudo yum install docker
 
 echo "Adding permissions"
-sudo usermod -a -G docker ec2-user
+sudo usermod -a -G docker ec2-user # Have the script detect the name of the current user
 
 echo "Installing docker-compose"
 sudo wget https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)
